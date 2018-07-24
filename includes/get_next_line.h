52a904a558d47e558a976a3c5c79b8abbb85cd5d
Gabrielle Kuraite <gabriellekuraite@MBPdeGabrielle.lan>
2018-07-24 14:48:57 +0200

    normed

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -13,11 +13,11 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 1
-	# include "libft.h"
-	# include <stdlib.h>
-	# include <limits.h>
-	# include <fcntl.h>
+# include "libft.h"
+# include <stdlib.h>
+# include <limits.h>
+# include <fcntl.h>
 
-	int		get_next_line(const int fd, char **line);
+int		get_next_line(const int fd, char **line);
 
 #endif
